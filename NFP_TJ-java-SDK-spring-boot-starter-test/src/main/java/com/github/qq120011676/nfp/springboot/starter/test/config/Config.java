2757148f5601a9ebc4a0aa277d09067952f6d346
Say.li <120011676@qq.com>
2021-11-18 17:32:40 +0800

    增加测试代码

---
+++ NFP_TJ-java-SDK-spring-boot-starter-test/src/main/java/com/github/qq120011676/nfp/springboot/starter/test/config/Config.java
@@ -0,0 +1,10 @@
+package com.github.qq120011676.nfp.springboot.starter.test.config;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+public class Config {
+    private String name = "abc";
+}
--- NFP_TJ-java-SDK-spring-boot-starter-test/src/main/java/com/github/qq120011676/nfp/springboot/starter/test/controller/TestController.java
+++ NFP_TJ-java-SDK-spring-boot-starter-test/src/main/java/com/github/qq120011676/nfp/springboot/starter/test/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.github.qq120011676.nfp.springboot.starter.test.controller;
 
+import com.github.qq120011676.nfp.springboot.starter.test.config.Config;
 import com.github.qq120011676.nfptj.NFPTJ;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,14 @@ import javax.annotation.Resource;
 @RestController
 @RequestMapping("test")
 public class TestController {
+    @Resource
+    private Config config;
+
+    @RequestMapping("config")
+    public Config config() {
+        return config;
+    }
+
     @Resource
     private NFPTJ nfptj;
 
